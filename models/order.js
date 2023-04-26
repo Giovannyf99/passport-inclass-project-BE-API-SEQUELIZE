11a0f0b027f035c1651250bb35adc2dbd7a0cfca
Giovannyf99 <giovannyf1999@gmail.com>
2023-04-26 13:43:28 -0400

    setup

---
+++ models/order.js
@@ -0,0 +1,34 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Order extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      class Order extends Model {
+        static associate(models) {
+          Order.belongsTo(models.User, {
+            foreignKey: 'user_id',
+          });
+          Order.belongsToMany(models.Basket, {
+            through: 'OrderBaskets',
+            foreignKey: 'order_id',
+          });
+        }
+      }
+    }
+  }
+  Order.init({
+    user_id: DataTypes.INTEGER,
+    ordered_date: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'Order',
+  });
+  return Order;
+};

31841163aee62d3fddf8e1e88891d6bb602ac71c
Giovannyf99 <giovannyf1999@gmail.com>
2023-04-26 14:07:47 -0400

    step 4.2

--- app.js
+++ app.js
@@ -2,17 +2,34 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session');
+const passport = require('passport');
+require('./config/passport')(passport);
+const authRouter = require('./routes/auth');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+app.use(
+    session({
+      secret: '1',
+      resave: false,
+      saveUninitialized: false,
+    })
+  );
+  app.use(passport.initialize());
+  app.use(passport.session());
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/auth', authRouter);
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
